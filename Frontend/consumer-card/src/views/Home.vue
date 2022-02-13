b092c04e111cec1b5cdf752a9949ca5f33835762
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2022-02-13 20:55:41 +0100

    feat: frontend setup

---
+++ Frontend/consumer-card/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="max-width:50%; text-align: center;margin: auto; margin-top:10% ">
+  
+  <div  class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Korisničko ime</label>
+    <input v-model="username" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Lozinka</label>
+    <input type="password" v-model="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary" v-on:click="signIn()">Prijavi se</button>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+  
+  },
+  data: function(){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods:{
+    signIn:function(){
+      console.log(this.username)
+      this.$router.push('/about');
+    }
+  }
+}
+</script>
